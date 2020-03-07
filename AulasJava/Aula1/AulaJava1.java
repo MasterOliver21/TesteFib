d673322a4b7cf0be0b22564c81748ed7609cf779
MasterOliver21 <matheusvini51@gmail.com>
2020-03-06 22:16:53 -0300

    att2

--- AulasJava/Aula1/AulaJava1.java
+++ AulasJava/Aula1/AulaJava1.java
@@ -1,6 +1,27 @@
 public class AulaJava1
 {
     public static void main(String arg []){
+        
+        double valor1 = 10.67;
+        
+        double valor2 = 52.98;
+        
+        double resultado = valor1 + valor2;
+        
+        
+        
         System.out.println("Hello World");
+        System.out.println("Aula Java 1");
+        
+        System.out.println("O resultado é: " + resultado);
+        
+        if(resultado >= 100){
+            System.out.println("O resultado é maior ou igual a 100.");
+            
+        }
+        else{
+            System.out.println("O resultado é menor do que 100.");
+        }
+        
     }
 }
---
+++ AulasJava/Aula1/Exercicio.java
@@ -0,0 +1,18 @@
+
+public class Exercicio
+{
+    public static void main(String arg []){
+        double alc = 2.50, gas = 3.99;
+        double resul = alc/gas;
+        
+        System.out.println("");
+        if(resul > 0.7){
+            System.out.println("A gasolina é mais supimpa");
+            
+        }
+        else{
+            System.out.println("O alcool is the bichao");
+        }
+        
+    }
+}
