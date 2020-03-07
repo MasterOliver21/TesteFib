43924b6166617458b63109ce387c276769322f08
MasterOliver21 <matheusvini51@gmail.com>
2020-03-06 22:21:50 -0300

    att3

---
+++ AulasJava/Aula1/Exercicio2.java
@@ -0,0 +1,17 @@
+public class Exercicio2
+{
+    public static void main(String arg []){
+        double m1 = 4.50, m2 = 1.00;
+        double resul = alc/gas;
+        
+        System.out.println("");
+        if(resul > 0.7){
+            System.out.println("A gasolina é mais supimpa");
+            
+        }
+        else{
+            System.out.println("O alcool is the bichao");
+        }
+        
+    }
+}
